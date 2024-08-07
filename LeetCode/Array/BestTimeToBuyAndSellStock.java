80eafb1c4001b5337f39611a2384600b0783a79f
Ragavan Sellamuthu <ragavansellamuthu.techymeet@gmail.com>
2024-08-07 08:34:28 +0530

    Problem Number : 121
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/?envType=study-plan-v2&envId=top-interview-150

--- LeetCode/Array/BestTimeToBuyAndSellStock.java
+++ LeetCode/Array/BestTimeToBuyAndSellStock.java
@@ -16,11 +16,8 @@ public class BestTimeToBuyAndSellStock {
 		int buy = prices[0];
 		int profit = 0;
 		for (int i = 1; i < prices.length; i++) {
-			if (prices[i] < buy) {
-				buy = prices[i];
-			} else if (prices[i] - buy > profit) {
-				profit = prices[i] - buy;
-			}
+			buy = Math.min(buy, prices[i]);
+			profit = Math.max(profit, prices[i]-buy);
 		}
 		return profit;
 	}
