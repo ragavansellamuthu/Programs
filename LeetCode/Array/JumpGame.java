f75173859a9ed81f5e0b360c13abae07de58f6e4
Ragavan Sellamuthu <ragavansellamuthu.techymeet@gmail.com>
2024-08-07 08:12:39 +0530

    Problem Number : 55
    https://leetcode.com/problems/jump-game/description/?envType=study-plan-v2&envId=top-interview-150

--- LeetCode/Array/JumpGame.java
+++ LeetCode/Array/JumpGame.java
@@ -3,8 +3,8 @@ package programs.leetcode.array;
 public class JumpGame {
 
 	public static void main(String[] args) {
-		int[] nums = { 2,3,1,1,4};
-		if(canJump(nums)) {
+		int[] nums = { 2, 3, 1, 1, 4 };
+		if (canJump(nums)) {
 			System.out.println("Reachable");
 		} else {
 			System.out.println("Not Reachable");
@@ -12,19 +12,16 @@ public class JumpGame {
 	}
 
 	public static boolean canJump(int[] nums) {
-		int reach = 0 ; 
-		for(int i=0;i<nums.length;i++) {
-			if(reach<i) {
+		int reach = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (reach < i) {
 				return false;
-			}
-			if(nums[i]+i>reach) {
-				reach = nums[i]+i;
-			}
-			if(reach>=nums.length-1) {
+			} else if (reach >= nums.length) {
 				return true;
 			}
+			reach = Math.max(reach, i + nums[i]);
 		}
-		return false;
+		return true;
 	}
 
 }
