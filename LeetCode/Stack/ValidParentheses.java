834ac6f4339d0fe8c5d7e0e500515bd5ee573156
Ragavan Sellamuthu <ragavansellamuthu.techymeet@gmail.com>
2024-08-08 07:32:24 +0530

    Problem Number : 20
    https://leetcode.com/problems/valid-parentheses/

--- LeetCode/Stack/ValidParentheses.java
+++ LeetCode/Stack/ValidParentheses.java
@@ -1,1 +1,48 @@
+package programs.leetcode.stack;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+
+	public static void main(String[] args) {
+
+		String brackets = "([}}])";
+
+		if (isValid(brackets)) {
+			System.out.println("Valid");
+		} else {
+			System.out.println("InValid");
+		}
+
+	}
+
+	public static boolean isValid(String brackets) {
+
+		if (brackets.length() % 2 != 0) {
+			return false;
+		}
+
+		Map<Character, Character> map = new HashMap<>();
+		map.put('(', ')');
+		map.put('[', ']');
+		map.put('{', '}');
+
+		Stack<Character> openBrackets = new Stack<Character>();
+
+		for (char bracket : brackets.toCharArray()) {
+			if (map.containsKey(bracket)) {
+				openBrackets.push(bracket);
+			} else if (!openBrackets.isEmpty() && map.get(openBrackets.peek()) == bracket) {
+				openBrackets.pop();
+			} else {
+				return false;
+			}
+		}
+
+		return openBrackets.isEmpty();
+	}
+
+}
 
